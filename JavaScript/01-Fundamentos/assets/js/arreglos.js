81034728fb9e496f06623ad1c24451eb0967e74f
Rafita75 <estudiosjuarez24@gmail.com>
2024-10-03 21:46:29 -0600

    Arreglos

---
+++ JavaScript/01-Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,27 @@
+//const arr = new Array(10);
+
+// const arr = [];
+// console.log(arr);
+
+let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log( {videoJuegos} );
+
+console.log( videoJuegos[0] );
+
+let arregloCosas = [
+    true,
+    123,
+    'Angel',
+    1 + 2,
+    function(){},
+    () => {},
+    {a : 1},
+    ['X', 'Flash', 'SpiderMan', [
+        'Iris',
+        'Cisco'
+    ]],
+];
+
+console.log( {arregloCosas} );
+
+console.log( arregloCosas[7][3][1] );
